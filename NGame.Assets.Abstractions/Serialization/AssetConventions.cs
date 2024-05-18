52a841d1922b786ae8c94f31de5d1dc1ca9e3781
Raphael Schmitz <49372721+raphaelschmitz00@users.noreply.github.com>
2024-05-18 19:04:13 +0200

    Add Prefab ID recognition (#8)
    * Update Assets.Abstractions
    
    * Update Assets.UsageFinder
    
    * Update Assets.ResolveUsedTask
    
    * Update Assets.Packing

--- NGame.Assets.Abstractions/Serialization/AssetConventions.cs
+++ NGame.Assets.Abstractions/Serialization/AssetConventions.cs
@@ -5,6 +5,7 @@ namespace NGame.Assets.Serialization;
 public static class AssetConventions
 {
 	public const string TypeDiscriminatorPropertyName = "$asset";
+	public const string PrefabIdPropertyName = "$prefabId";
 	public const string AssetIdPropertyName = "Id";
 	public const string AssetFileEnding = ".ngasset";
 	public const string RelativePathSeparator = "//";
